a8dd2a4716b25becf0e80d58bee5b72edff949e1
Alejandro Martinez <alejandro@Alejandros-MacBook-Pro.local>
2025-05-20 18:19:00 -0500

    testing changes

--- frontend/src/components/HelloWorld.vue
+++ frontend/src/components/HelloWorld.vue
@@ -13,8 +13,6 @@ defineProps<{
       <a href="https://vite.dev/" target="_blank" rel="noopener">Vite</a> +
       <a href="https://vuejs.org/" target="_blank" rel="noopener">Vue 3</a>.
     </h3>
-    sdjndjknskjdn
-
     <TheWelcome />
   </div>
 </template>
